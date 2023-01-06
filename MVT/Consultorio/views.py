db5c4c602d1aa8288b0bbbf4efbd69c4d9c80404
Nachorvw <nachorewe@gmail.com>
2023-01-06 19:25:49 -0300

    views a hacer

--- MVT/Consultorio/views.py
+++ MVT/Consultorio/views.py
@@ -3,3 +3,20 @@ from Consultorio.models import Patient, Orders, Medicines
 
 # Create your views here.
 
+def patient_creation(request):
+    pass
+
+def patient_list(request):
+    pass
+
+def order_creation(request):
+    pass
+
+def order_list(request):
+    pass
+
+def medicine_creation(request):
+    pass
+
+def medicine_list(request):
+    pass
