818c677ffa94a4ad2e73467cdbdb6306fef19f6f
Nachorvw <nachorewe@gmail.com>
2023-02-08 11:31:02 -0300

    django urls in html, fixed footer

--- MVT/MVT/urls.py
+++ MVT/MVT/urls.py
@@ -23,30 +23,30 @@ from MVT.settings import MEDIA_ROOT, MEDIA_URL
 from django.conf.urls.static import static
 urlpatterns = [
     path("", index, name="index"),
-    path('admin/', admin.site.urls),
-    path("about-us/", about_us),
+    path('admin/', admin.site.urls,),
+    path("about-us/", about_us, name="about-us"),
     #---patients---
-    path("create-patient/", patient_creation),
-    path("list-patients/", patient_list),
-    path("update-patient/<int:pk>/", PatientUpdate.as_view()),
-    path("delete-patient/<int:pk>/", PatientDelete.as_view()),
-    path('profile/<int:pk>/', patientdetail),
+    path("create-patient/", patient_creation, name="patient-create"),
+    path("list-patients/", patient_list, name="patient-list"),
+    path("update-patient/<int:pk>/", PatientUpdate.as_view(), name="patient-update"),
+    path("delete-patient/<int:pk>/", PatientDelete.as_view(), name="patient-delete"),
+    path('profile/<int:pk>/', patientdetail, name="patient-profile"),
     #---orders---
-    path("create-order/<int:pk>/", order_creation),
-    path("list-orders/", order_list),
-    path("update-order/<int:pk>/", OrderUpdate.as_view()),
-    path("delete-order/<int:pk>/", OrderDelete.as_view()),
+    path("create-order/<int:pk>/", order_creation, name="order-create"),
+    path("list-orders/", order_list, name="order-list"),
+    path("update-order/<int:pk>/", OrderUpdate.as_view(), name="order-update"),
+    path("delete-order/<int:pk>/", OrderDelete.as_view(), name="order-delete"),
     #---medicines---
-    path("create-medicine/", medicine_creation),
-    path("list-medicines/", medicine_list),
-    path("update-medicine/<int:pk>/", MedicineUptade.as_view()),
-    path("delete-medicine/<int:pk>/", MedicineDelete.as_view()),
+    path("create-medicine/", medicine_creation, name="medicine-create"),
+    path("list-medicines/", medicine_list, name="medicine-list"),
+    path("update-medicine/<int:pk>/", MedicineUptade.as_view(), name="medicine-update"),
+    path("delete-medicine/<int:pk>/", MedicineDelete.as_view(), name="medicine-delete"),
     #---user---
     path("login/", user_login, name="login"),
     path("logout/", LogoutView.as_view(template_name = "users/logout.html")),
-    path("register/", user_register),
-    path("update-user/", update_user),
-    path("update-profile/", update_user_profile),
-    path("user-profile/", user_profile_page)
+    path("register/", user_register, name="register"),
+    path("update-user/", update_user, name="user-update"),
+    path("update-profile/", update_user_profile, name="profile-update"),
+    path("user-profile/", user_profile_page, name="user-profile")
 
 ] + static(MEDIA_URL, document_root = MEDIA_ROOT)
